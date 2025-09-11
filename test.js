b54f4228a95efde0c99ba41fb7931f19fc06073b
Anushiya <anushiya96@gmail.com>
2025-09-11 15:04:07 +0800

    Add new changes in feature1

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-console.log("Hello World");
+//this will only show in the console tab in Inspect element, not on the webpage
+console.log("Hello from the Console World");
+//alert("This alert box was called with the onload event");
