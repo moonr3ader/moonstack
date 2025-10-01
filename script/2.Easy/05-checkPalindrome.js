07b466bb10c81e4d873198f441798169b7e0a3d7
Anushiya <anushiya96@gmail.com>
2025-10-01 23:55:34 +0800

    basic maths practice

--- script/2.Easy/05-checkPalindrome.js
+++ script/2.Easy/05-checkPalindrome.js
@@ -2,10 +2,18 @@
 
 function isPalindrome(n){
     let reverse = 0;
+    while(n > 0){
+        reverse = reverse * 10 + n % 10;
+        n = Math.floor(n/10);
+    }
+    if(n === reverse) return true;
+    else return false;
+}
+
+let n = [151, 132, 555, 141, 12];
+for(let val of n){
     
 }
 
-let n = 151;
-let n2 = 132;
 console.log(isPalindrome(n));
 console.log(isPalindrome(n2));
