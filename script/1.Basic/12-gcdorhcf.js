4468804228352413df21e610c8d641dc900f9ab3
Anushiya <anushiya96@gmail.com>
2025-10-14 23:21:20 +0800

    practice basic-gcd/hcf problem

--- script/1.Basic/12-gcdorhcf.js
+++ script/1.Basic/12-gcdorhcf.js
@@ -1,1 +1,16 @@
-12-gcdorhcf.js
+function gcd1(a, b){
+    let result = Math.min(a, b);
+
+    while(result > 0){
+        if(a % result === 0 && b % result === 0){
+            break;
+        }
+        result--;
+    }
+
+    return result;
+}
+
+let a = 20;
+let b = 28;
+console.log(gcd1(a,b));
