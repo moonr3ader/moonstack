7ee465df7fb43d64aa810b8ed6f27442a0701e2f
Anushiya <anushiya96@gmail.com>
2025-10-18 23:26:15 +0800

    practice linear searching

--- script/1.Basic/15-insertAtEnd.js
+++ script/1.Basic/15-insertAtEnd.js
@@ -8,16 +8,3 @@ function insertAtEnd(arr, val){
 let arr = [16,54,86,35];
 let val = 48;
 console.log(insertAtEnd(arr, val));
-
-
-function linearSearchArr(arr, val){
-    for(let i=0;i<arr.length;i++){
-        if(arr[i] === val){
-            return i;
-        }
-    }
-    return -1;
-}
-
-arr = [15, 15, 64, 86, 10, 13, 1, 6, 48, 9, 5];
-console.log(`The value ${val} is at index ${linearSearchArr(arr,val)} in the array.`);
---
+++ script/1.Basic/linearSearching.js
@@ -0,0 +1,62 @@
+/*
+What is linear searching: Searching something from left to right in a data structure 
+( like arrays, objects, and strings).
+*/
+
+// BASIC LINEAR SEARCH
+// search the array to see if val exist or not
+function searchArrays(arr, val){
+    
+    if (arr.length === 0) return -1;
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] === val){
+            return i;
+        }
+    }
+    return -1;
+
+    // TIME COMPLEXITY: O(n)
+    // SPACE COMPLEXITY: O(1)
+}
+
+function searchArrays2(arr, val){
+    if(arr.lenght === 0) return -1;
+
+    for(let [i, item] of arr.entries()){
+        if(Object.is(arr[i], val)) return i;
+    }
+}
+
+let arr = [15, 15, 64, 86, 10, 13, 1, 6, 48, 9, 5];
+let val = 6;
+
+const idx = searchArrays(arr,val);
+
+if (idx !== -1) {
+  console.log(`"${val}" found at index ${idx}.`);
+} else {
+  console.log(`"${val}" not found in the array.`);
+}
+
+// linear searching in string
+function searchStr(str, ch){
+    
+    if(str.length === 0) return -1;
+    
+    for(let i=0;i<str.length;i++){
+        if(str[i] === ch){
+            return i;
+        }
+    }
+    return -1;
+}
+let str = "GeeksForGeeks";
+let ch = 'f';
+const index = searchStr(str, ch);
+
+if (index !== -1) {
+  console.log(`"${ch}" found at index ${index}.`);
+} else {
+  console.log(`"${ch}" not found in the array.`);
+}
