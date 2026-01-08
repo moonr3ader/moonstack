f94bb635d737a7be33708f85e87750f7cb4af066
moonr3ader <ANUSHIYA96@GMAIL.COM>
2026-01-08 21:12:55 +0800

    Implement binary search function and test code
    Added a binary search function to find the index of an element in a sorted array. Included driver code to test the binary search functionality.

--- script/2.Easy/10-searchInsertK.js
+++ script/2.Easy/10-searchInsertK.js
@@ -47,7 +47,42 @@ let arr = [2, 6, 7, 10, 14], k = 10;
 let arr2 = [2, 6, 10, 14];
 console.log(searchInsertK1(arr, k));
 
+function binarySearch(arr, x) {
+    let low = 0;
+    let high = arr.length - 1;
+    let mid;
+    while (high >= low) {
+        mid = low + Math.floor((high - low) / 2);
 
+        // If the element is present at the middle
+        // itself
+        if (arr[mid] == x)
+            return mid;
+
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (arr[mid] > x)
+            high = mid - 1;
+
+        // Else the element can only be present
+        // in right subarray
+        else
+            low = mid + 1;
+    }
+
+    // We reach here when element is not
+    // present in array
+    return -1;
+}
+
+// Driver Code
+arr = new Array(2, 3, 4, 10, 40);
+x = 10;
+result = binarySearch(arr, x);
+if (result == -1)
+    console.log("Element is not present in array")
+else
+    console.log("Element is present at index " + result);
 
 
 
