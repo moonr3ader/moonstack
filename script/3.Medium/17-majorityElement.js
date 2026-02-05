f712458da4c061220ea29ba1f69769ef7662653c
moonr3ader <ANUSHIYA96@GMAIL.COM>
2026-02-05 22:15:08 +0800

    Add majorityElement function to find dominant element
    Implement majority element algorithm to find the element that appears more than n/2 times in an array using Moore's Voting Algorithm

---
+++ script/3.Medium/17-majorityElement.js
@@ -0,0 +1,40 @@
+function majorityElement(arr) {
+
+    const n = arr.length;
+    let candidate = -1;
+    let count = 0;
+
+    // Find a candidate
+    for (const num of arr) {
+        if (count === 0) {
+            candidate = num;
+            count = 1;
+        } 
+        else if (num === candidate) {
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+
+    // Validate the candidate
+    count = 0;
+    for (const num of arr) {
+        if (num === candidate) {
+            count++;
+        }
+    }
+	
+    // If count is greater than n / 2, return 
+    // the candidate; otherwise, return -1
+    if (count > n / 2) {
+        return candidate;
+    } else {
+        return -1;
+    }
+}
+
+// Driver Code 
+const arr = [1, 1, 2, 1, 3, 5, 1];
+console.log(majorityElement(arr));
