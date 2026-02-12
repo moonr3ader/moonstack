0a1bf3adf135730b476fbb367fff256842e352c7
Anushiya <anushiya96@gmail.com>
2026-02-12 21:24:13 +0800

    learning Kadane algorithm and notes

--- script/3.Medium/08-maxSubarraySum.js
+++ script/3.Medium/08-maxSubarraySum.js
@@ -1,18 +1,40 @@
 // Using Kadane's Algorithm - O(n) Time and O(1) Space
 
-function maxSubarraySum(arr) {
+// HOW TO GENERATE ALL SUBARRAYS?
+
+let getSubArr = (arr) => {
+    // Empty array to store one subarray
+    let arr1 = [];
+    for(let i=0;i<arr.length; i++){
+        // Empty array, to store one subarray
+        let subArr = [];
+        for(let j=i; j<arr.length; j++){
+            // to get elements to the right of selected in the element till j
+            subArr.push(arr.slice(i,j+1));
+        }
+        // storing individual subarrays into main array
+        arr1.push(subArr);
+    }
+    return arr1;
+}
+
+let arr = [1,2,3,4];
+let arr1 = getSubArr(arr);
+console.log(arr1);
+
+function maxSubarraySum(arr2) {
     
     // Stores the result (maximum sum found so far)    
-    let res = arr[0];
+    let res = arr2[0];
     
     // Maximum sum of subarray ending at current position
-    let maxEnding = arr[0];
+    let maxEnding = arr2[0];
 
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr2.length; i++) {
         
         // Either extend the previous subarray or start 
         // new from current element
-        maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+        maxEnding = Math.max(maxEnding + arr2[i], arr2[i]);
         
         // Update result if the new subarray sum is larger
         res = Math.max(res, maxEnding);
@@ -21,5 +43,5 @@ function maxSubarraySum(arr) {
 }
 
 // Driver Code
-const arr = [2, 3, -8, 7, -1, 2, 3];
-console.log(maxSubarraySum(arr));
+const arr2 = [2, 3, -8, 7, -1, 2, 3];
+console.log(maxSubarraySum(arr2));
---
+++ script/handbooks/subarray.md
@@ -0,0 +1,6 @@
+What is Subarray/SubString?
+- Official meaning is that its the contiguous part of the array, but for easier understanding its basically its like subsets. For example, you got a array like [1,2,3,4], how many subsets you can get from this?
+    - [1], [2], [3], [4], [1,2], [2,3],[3,4], [1,2,3], [2,3,4] and [1,2,3,4].
+IN TOTAL: 10 non-empty subarrays.
+
+Check out how to derive all the subarrays in 3.Medium/08-maxSubarraySum.js file.
