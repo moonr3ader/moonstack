0118c18da37361ec177d99a06cbc9e09a9c852d4
Anushiya <anushiya96@gmail.com>
2026-03-11 20:57:00 +0800

    installed backend practice

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+// API ROUTE
+app.get("/api/message", (req, res) => {
+    res.json({message: "Hello from chaicode server"});
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+});
